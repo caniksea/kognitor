e00e073bded3b24b798b994b84bb6cd23ed3c5ce
boniface <boniface@kabaso.com>
2019-02-09 06:23:56 +0200

    Merge branch 'master' of https://github.com/boniface/kognitor
    # Conflicts:
    #	build.sbt
    #	project/build.properties
    #	project/plugins.sbt

---
+++ app/services/actors/ProcessInputActor.scala
@@ -0,0 +1,26 @@
+package services.actors
+
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.routing.DefaultResizer
+
+
+object ProcessInputActor{
+
+  def props: Props = Props[ProcessInputActor]
+}
+
+class ProcessInputActor  extends Actor with ActorLogging{
+
+  val resizer: DefaultResizer = DefaultResizer(lowerBound = Util.config.getInt("actorNumbers.lowerBound"), upperBound = Util.config.getInt("actorNumbers.upperBound"))
+
+  override def receive: Receive = {
+
+
+    case "START" => {
+
+
+    }
+    case _ => println(" This is Unknow Message ")
+  }
+
+}
