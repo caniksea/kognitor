29979a56324c8e59a6eb7a0738a25545cfa13e7b
caniksea <caniksea@yahoo.co.nz>
2019-02-14 18:07:43 +0200

    ==> Started some work on the crawler component Updated the readme.me

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 2. Add Streams Lib
 3.Updated Libs 
 
+## Data sources
+####You can go back in year for historic data
+1. https://www.whoscored.com/Regions/252/Tournaments/2/Seasons/7361/Stages/16368/TeamStatistics/England-Premier-League-2018-2019
+2. https://www.squawka.com/en/football-team-rankings
+3. https://www.premierleague.com/stats/head-to-head
+4. https://www.premierleague.com/stats/top/clubs/wins?se=210
+
+(look at: ~/component/feeder/CrawlerComponent)
--- app/component/feeder/CrawlerComponent.scala
+++ app/component/feeder/CrawlerComponent.scala
@@ -3,12 +3,34 @@ package component.feeder
 import domain.feeder.AppDatasource
 import services.feeder.AppDatasourceService
 
+import com.softwaremill.sttp._
+import com.softwaremill.sttp.playJson._
+import com.softwaremill.sttp.asynchttpclient.future.AsyncHttpClientFutureBackend
+import conf.connections.NetConnection
+import domain.datainjection.Post
+import play.api.libs.json.Json
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object CrawlerComponent {
 
-  def crawl(source: String) = {
+  implicit lazy val sttpBackend = AsyncHttpClientFutureBackend()
+  lazy val className = "CrawlerComponent "
+  lazy val conn: NetConnection.type = NetConnection
+  lazy val secure: Boolean = conn.schema
+  val scheme: String = if (secure) "https" else "http"
 
+  def crawl(source: String) = {
+    lazy val url = uri"${source}"
+    val apiResponse: Future[Response[String]] =
+      sttp
+        .post(url)
+//        .body(data)
+        .response(asString)
+        .send()
+    val response = apiResponse map (result => Json.fromJson[Post](Json.parse(result.unsafeBody)).asEither)
+    response map (result => processPost(source, result))
   }
 
   def crawlSources(sources: Seq[AppDatasource]): Boolean = {
